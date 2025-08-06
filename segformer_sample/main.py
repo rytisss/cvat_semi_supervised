caf6379aafcf0074ce6782a1d30da0d5d3d9fe3e
rytis <rytis.here@gmail.com>
2025-08-06 23:05:46 +0300

    Function prediction fix

--- segformer_sample/main.py
+++ segformer_sample/main.py
@@ -1,4 +1,6 @@
 import io
+import json
+import yaml
 import torch
 import cv2
 import numpy as np
@@ -7,37 +9,48 @@ import segmentation_models_pytorch as smp
 from PIL import Image
 import base64
 
+def get_label_name_by_id(context, label_id):
+    for label in context.user_data.labels:
+        if label["id"] == label_id:
+            return label["name"]
+    return None
+
 def init_context(context):
     context.logger.info("Initializing segmentation model...")
 
     # Load model and preprocessing only once
     checkpoint = "smp-hub/segformer-b5-640x640-ade-160k"
-    context.model = smp.from_pretrained(checkpoint).eval()
-    context.preprocessing = A.Compose.from_pretrained(checkpoint)
+    context.user_data.model = smp.from_pretrained(checkpoint).eval()
+    context.user_data.preprocessing = A.Compose.from_pretrained(checkpoint)
+
+    with open("/opt/nuclio/function.yaml") as f:
+        cfg = yaml.safe_load(f)
+    context.user_data.labels = json.loads(cfg["metadata"]["annotations"]["spec"])
 
 def handler(context, event):
+    context.logger.info("Requst received")
     # Get the image data from the event (list of images)
     data = event.body
+    buf = io.BytesIO(base64.b64decode(data["image"]))
+    threshold = float(data.get("threshold", 0.5))
+    #context.user_data.model.conf = threshold
+    image = np.array(Image.open(buf))
+    context.logger.info("Image decoded")
+    # Preprocess
+    context.logger.info(f'{type(image)}')
+    image_preprocessed = context.user_data.preprocessing(image=image)["image"]
+    input_tensor = torch.as_tensor(image_preprocessed).permute(2, 0, 1).unsqueeze(0)
 
-    results = []
-    for item in data:
-        image_data = base64.b64decode(item["image"])
-        image = np.array(Image.open(io.BytesIO(image_data)).convert("RGB"))
-
-        # Preprocess
-        image_preprocessed = context.preprocessing(image=image)["image"]
-        input_tensor = torch.as_tensor(image_preprocessed).permute(2, 0, 1).unsqueeze(0)
-
-        # Inference
-        with torch.no_grad():
-            output = context.model(input_tensor)
-
-        # Resize mask to original size
-        mask = torch.nn.functional.interpolate(
-            output, size=(image.shape[0], image.shape[1]),
-            mode="bilinear", align_corners=False
-        )
-        mask = mask.argmax(1).squeeze().cpu().numpy()
+    # Inference
+    with torch.no_grad():
+        output = context.user_data.model(input_tensor)
+    context.logger.info("Prediction made")
+    # Resize mask to original size
+    mask = torch.nn.functional.interpolate(
+        output, size=(image.shape[0], image.shape[1]),
+        mode="bilinear", align_corners=False
+    )
+    mask = mask.argmax(1).squeeze().cpu().numpy()
 
     results = []
     for class_id in np.unique(mask):
@@ -59,12 +72,17 @@ def handler(context, event):
 
         polygon = [[int(x), int(y)] for x, y in largest]
 
+        label_name = get_label_name_by_id(context, class_id)
+        context.logger.info(f'Detected {label_name}, {class_id}')
         results.append({
+            "confidence" : str(threshold),
             "label": str(class_id),
             "points": polygon,
+            "type" : "mask",
             "mask": binary_mask.tolist()
         })
 
+        
     return context.Response(
         body=json.dumps(results),
         headers={},
